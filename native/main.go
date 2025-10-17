add5e79430ba27ea1cd8cfadd8f7db23d97dfe20
wxn0brP <toja.filiper@gmail.com>
2025-10-18 00:18:20 +0200

    feat: update native

--- native/main.go
+++ native/main.go
@@ -3,29 +3,35 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
-    webview "github.com/lexesv/go-webview-gui"
+	"strings"
+	webview "github.com/lexesv/go-webview-gui"
 )
 
 func main() {
-	port := 8080
 	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s [port] [title]\n", os.Args[0])
+		fmt.Printf("Usage: %s <port>\nExample: %s 8080\n", os.Args[0], os.Args[0])
 		return
 	}
-	if p, err := strconv.Atoi(os.Args[1]); err == nil {
-		port = p
-	}
-	title := "wxn0brP"
-	if len(os.Args) > 2 {
-		title = os.Args[2]
+	target := os.Args[1]
+
+	if !strings.Contains(target, "://") {
+		target = "http://localhost:" + target
 	}
 
-    w := webview.New(true, true)
-    defer w.Destroy()
-    w.SetTitle(title)
-    w.SetSize(1600, 900, webview.HintNone)
+	w := webview.New(true, true)
+	defer w.Destroy()
+	w.SetTitle("wxn0brP")
+	w.SetSize(1600, 900, webview.HintNone)
 	w.Maximize()
-    w.Navigate(fmt.Sprintf("http://localhost:%d", port))
-    w.Run()
+
+	w.Bind("zhiva_setWindowTitle", func(newTitle string) {
+		w.Dispatch(func() {
+			w.SetTitle(newTitle)
+		})
+	})
+	w.Bind("zhiva_isApp", func() bool { return true })
+	w.Bind("zhiva_closeApp", func() { w.Dispatch(w.Terminate) })
+
+	w.Navigate(target)
+	w.Run()
 }
